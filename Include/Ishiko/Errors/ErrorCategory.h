32718a4ddb99fbb5b1d5f5743260b35f38d3bc6c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-01 11:33:59 +0100

    Add ErrorCategory class

---
+++ Include/Ishiko/Errors/ErrorCategory.h
@@ -0,0 +1,19 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Errors/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_ERRORS_ERRORCATEGORY_H_
+#define _ISHIKO_ERRORS_ERRORCATEGORY_H_
+
+namespace Ishiko
+{
+
+class ErrorCategory
+{
+};
+
+}
+
+#endif
---
+++ Include/Ishiko/Errors/IOErrorCategory.h
@@ -0,0 +1,21 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Errors/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_ERRORS_IOERRORCATEGORY_H_
+#define _ISHIKO_ERRORS_IOERRORCATEGORY_H_
+
+#include "ErrorCategory.h"
+
+namespace Ishiko
+{
+
+class IOErrorCategory : public ErrorCategory
+{
+};
+
+}
+
+#endif
