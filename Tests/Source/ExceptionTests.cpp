2adbf8b511cc037348bf79b5effd245d41545838
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-08 22:02:37 +0100

    Add ErrorCondition to Exception

--- Tests/Source/ExceptionTests.cpp
+++ Tests/Source/ExceptionTests.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "ExceptionTests.h"
+#include "Helpers/TestErrorCategory1.h"
 #include "Ishiko/Errors/Exception.h"
 
 using namespace Ishiko::Tests;
@@ -13,14 +14,53 @@ ExceptionTests::ExceptionTests(const TestNumber& number, const TestEnvironment& 
     : TestSequence(number, "Exception tests", environment)
 {
     append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+    append<HeapAllocationErrorsTest>("Constructor test 2", ConstructorTest2);
+    append<HeapAllocationErrorsTest>("Constructor test 3", ConstructorTest3);
+    append<HeapAllocationErrorsTest>("Constructor test 4", ConstructorTest4);
 }
 
 void ExceptionTests::ConstructorTest1(Test& test)
 {
-    Ishiko::Exception exception("description", "file", 5);
+    Ishiko::Exception exception(-1, TestErrorCategory1::Get(), "file", 5);
 
+    ISHTF_FAIL_IF_NEQ(exception.condition().value(), -1);
+    ISHTF_FAIL_IF_STR_NEQ(exception.what(), "");
+    ISHTF_FAIL_IF_NEQ(exception.file(), "file");
+    ISHTF_FAIL_IF_NEQ(exception.line(), 5);
+    ISHTF_PASS();
+}
+
+void ExceptionTests::ConstructorTest2(Test& test)
+{
+    Ishiko::Exception exception(-1, TestErrorCategory1::Get(), "description", "file", 5);
+
+    ISHTF_FAIL_IF_NEQ(exception.condition().value(), -1);
     ISHTF_FAIL_IF_STR_NEQ(exception.what(), "description");
     ISHTF_FAIL_IF_NEQ(exception.file(), "file");
     ISHTF_FAIL_IF_NEQ(exception.line(), 5);
+    ISHTF_PASS();
+}
+
+void ExceptionTests::ConstructorTest3(Test& test)
+{
+    Ishiko::Exception exception1(-1, TestErrorCategory1::Get(), "file", 5);
+    Ishiko::Exception exception2(exception1);
+
+    ISHTF_FAIL_IF_NEQ(exception2.condition().value(), -1);
+    ISHTF_FAIL_IF_STR_NEQ(exception2.what(), "");
+    ISHTF_FAIL_IF_NEQ(exception2.file(), "file");
+    ISHTF_FAIL_IF_NEQ(exception2.line(), 5);
+    ISHTF_PASS();
+}
+
+void ExceptionTests::ConstructorTest4(Test& test)
+{
+    Ishiko::Exception exception1(-1, TestErrorCategory1::Get(), "description", "file", 5);
+    Ishiko::Exception exception2(exception1);
+
+    ISHTF_FAIL_IF_NEQ(exception2.condition().value(), -1);
+    ISHTF_FAIL_IF_STR_NEQ(exception2.what(), "description");
+    ISHTF_FAIL_IF_NEQ(exception2.file(), "file");
+    ISHTF_FAIL_IF_NEQ(exception2.line(), 5);
     ISHTF_PASS();
 }
