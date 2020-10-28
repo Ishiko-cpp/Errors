681a39f7593b63067dacb83e8a5d5d0b608b905a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-28 18:45:25 +0000

    Update Errors.h

--- Include/Ishiko/Errors.h
+++ Include/Ishiko/Errors.h
@@ -7,8 +7,16 @@
 #ifndef _ISHIKO_ERRORS_H_
 #define _ISHIKO_ERRORS_H_
 
+#include "Errors/ChainErrorExtension.h"
 #include "Errors/Error.h"
+#include "Errors/ErrorCategory.h"
+#include "Errors/ErrorCondition.h"
+#include "Errors/ErrorExtension.h"
 #include "Errors/Exception.h"
+#include "Errors/IOErrorCategory.h"
+#include "Errors/IOErrorExtension.h"
+#include "Errors/MessageErrorExtension.h"
+#include "Errors/SuccessCategory.h"
 
 #include "Errors/linkoptions.h"
 
