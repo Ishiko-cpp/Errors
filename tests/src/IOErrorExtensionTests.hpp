f704d8dd1bb0ab17a7d85f3d5f72dd21de099df5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-10 14:45:06 +0100

    Fix code after changes to dependencies (#69)

--- tests/src/IOErrorExtensionTests.hpp
+++ tests/src/IOErrorExtensionTests.hpp
@@ -7,7 +7,7 @@
 #ifndef _ISHIKO_CPP_ERRORS_TESTS_IOERROREXTENSIONTESTS_HPP_
 #define _ISHIKO_CPP_ERRORS_TESTS_IOERROREXTENSIONTESTS_HPP_
 
-#include <Ishiko/Tests/Core.hpp>
+#include <Ishiko/TestFramework/Core.hpp>
 
 class IOErrorExtensionTests : public Ishiko::TestSequence
 {
