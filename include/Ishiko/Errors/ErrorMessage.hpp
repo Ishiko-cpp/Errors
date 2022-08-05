a8e36d9ead8c555c338c2543571e7f155be8383d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-08-05 18:02:10 +0100

    Updates

--- include/Ishiko/Errors/ErrorMessage.hpp
+++ include/Ishiko/Errors/ErrorMessage.hpp
@@ -7,13 +7,53 @@
 #ifndef GUARD_ISHIKO_CPP_ERRORS_ERRORMESSAGE_HPP
 #define GUARD_ISHIKO_CPP_ERRORS_ERRORMESSAGE_HPP
 
+#include <ostream>
+#include <string>
+
 namespace Ishiko
 {
 
 class ErrorMessage
 {
+public:
+    inline bool empty() const noexcept;
+    inline size_t size() const noexcept;
+
+    ErrorMessage& assign(const std::string& str) noexcept;
+
+    inline bool operator==(const char* other) const noexcept;
+    inline bool operator!=(const char* other) const noexcept;
+
+    std::string toString() const;
+
+private:
+    friend std::ostream& operator<<(std::ostream& os, const ErrorMessage& error);
+
+    std::string m_data;
 };
 
+std::ostream& operator<<(std::ostream& os, const ErrorMessage& error);
+
+}
+
+bool Ishiko::ErrorMessage::empty() const noexcept
+{
+    return m_data.empty();
+}
+
+size_t Ishiko::ErrorMessage::size() const noexcept
+{
+    return m_data.size();
+}
+
+bool Ishiko::ErrorMessage::operator==(const char* other) const noexcept
+{
+    return (m_data == other);
+}
+
+bool Ishiko::ErrorMessage::operator!=(const char* other) const noexcept
+{
+    return !(*this == other);
 }
 
 #endif
--- src/ErrorMessage.cpp
+++ src/ErrorMessage.cpp
@@ -5,3 +5,27 @@
 */
 
 #include "ErrorMessage.hpp"
+
+using namespace Ishiko;
+
+ErrorMessage& ErrorMessage::assign(const std::string& str) noexcept
+{
+    try
+    {
+        m_data.assign(str);
+    }
+    catch (...)
+    {
+    }
+    return *this;
+}
+
+std::string ErrorMessage::toString() const
+{
+    return m_data;
+}
+
+std::ostream& Ishiko::operator<<(std::ostream& os, const ErrorMessage& error)
+{
+    return (os << error.m_data);
+}
